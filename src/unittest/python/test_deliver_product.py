d6907668f3bf1490cf7e488ab7d8789cfb14b536
NataaNK <100471976@alumnos.uc3m.es>
2023-03-26 11:39:08 +0200

    RF2: FUNCIÓN HECHA

--- src/unittest/python/test_deliver_product.py
+++ src/unittest/python/test_deliver_product.py
@@ -17,3 +17,9 @@ class TestDeliverProduct(TestCase):
         # CÓDIGO DE PRUEBA. PARA PROBAR EL FUNCIONAMIENTO DE RF3
         prueba = OrderManager()
         prueba.deliver_product("c7d0c3b0098a98d782981e6c5d7f5a2808dd0c6841dd12c1932e9ad9499b243c")
+
+        prueba2 = OrderManager()
+        prueba2.deliver_product("59adf8977b25f2ba3d4e989c51ac1f6dfce29544fe01bc84770f9afad42290ee")
+
+        prueba3 = OrderManager()
+        prueba3.deliver_product("ebf15a34d6451a7314a74ac395bfcc3d00e5e34a0fafe51fe9e1d0680928294d")
